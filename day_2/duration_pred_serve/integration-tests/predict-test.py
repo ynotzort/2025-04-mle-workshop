b46a7ff94e5a79f0db08d39a235da2c670ea09ad
ynotzort <ynotzort@users.noreply.github.com>
2025-04-04 10:16:59 +0000

    restore local url for predict-test

--- day_2/duration_pred_serve/integration-tests/predict-test.py
+++ day_2/duration_pred_serve/integration-tests/predict-test.py
@@ -1,7 +1,6 @@
 import requests
 
-# url = "http://127.0.0.1:9696/predict"
-url = "https://duration-pred-serve-2025-04.fly.dev/predict"
+url = "http://127.0.0.1:9696/predict"
 trip = {
 	"PULocationID": 43,
 	"DOLocationID": 238,
